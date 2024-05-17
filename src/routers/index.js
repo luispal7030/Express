2374c892231ce573e04d1a7875c0068e91bfecb6
literame <git config --global user.email>
2024-05-17 07:04:46 -0500

    paginas y ingreso de registros

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('index');
-    console.log("1")
+    
 })
 
 router.get('/about', (req, res) => {
--- src/routers/note.js
+++ src/routers/note.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/notes', (req, res) => {
-    res.send('notas desde la base de datos');
+const Note = require('../models/Note');
+
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-notes');
+})
+
+router.post('/notes/new-note', async (req, res) => {
+    const {producto, cantidad} = req.body;
+    const errors = [];
+    if(!producto){
+        errors.push({text: 'ingresa el nombre del producto'})
+    }
+    if(!cantidad){
+        errors.push({text: 'ingresa el cantidad del producto'})
+
+    }
+    if(errors.length > 0){
+        res.render('notes/new-notes', {
+            errors, producto, cantidad
+        })
+    }else{
+       const newNote = new Note({producto, cantidad});
+       await newNote.save();
+        res.redirect('/notes');
+    }
+})
+
+router.get('/notes', async (req, res) => {
+   const notes = await Note.find().sort({date: 'desc'});
+   res.render('notes/all-notes', {notes});
+   
 })
 module.exports = router;
